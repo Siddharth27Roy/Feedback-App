e64aa9cf712c97f6285861f0ef4f9557cfe9ed4b
Siddharth27Roy <siddharth27.roy27@gmail.com>
2024-06-12 00:24:11 +0530

    navBar and messageCard added

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster"
 import NavBar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,13 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
+
         <body className={inter.className}>
           <NavBar/>
           {children}
-          <Toaster />
+
         </body>
-      </AuthProvider>
+
     </html>
   );
 }
